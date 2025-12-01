5e5b1182f1d95c03bd6f5c723402106a0b799d81
njato <herilazanjato@gmail.com>
2025-12-01 18:59:18 +0300

    revers

--- src/components/display/Hero.tsx
+++ src/components/display/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="container">
       <div className="hero ">
         <div className="flex flex-col gap-2 max-w-2xs text-white">
-          <h1 className="text-3xl font-bold">ienvenue chez Quincaillerie</h1>
+          <h1 className="text-3xl font-bold">Bienvenue chez Quincaillerie</h1>
           <p>Votre partenaire de confiance en matière de quincaillerie</p>
           <Button
             variant="default"
